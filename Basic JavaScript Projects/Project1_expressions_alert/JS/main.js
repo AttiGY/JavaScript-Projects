e11077afd462d0cae85e3be640e9e74a1727c22e
AttiGY <attigy@live.com>
2023-03-14 18:26:16 +0100

    submitted basic JavaScript project 1

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,1 +1,23 @@
-document.write ("Hello, World!");
+document.write("Hellow world! <br>"); //displaying string
+
+document.write(" I was not convinced when he said \"he didn't like the idea\"<br>"); //displaying string with ' or "
+
+//creating variables
+var Sent1 = "This is the beining of the string" 
+var Sent2 = " and this is the end of the string <br>";
+document.write (Sent1+Sent2) //displaying variables together
+
+
+window.alert ("Bacis JavaScript Projects Assignment"); //displays text in the () in alert window
+
+//Concatenating strings
+document.write (" \"The story so far:"
+    + " In the beginning the Universe was created."
+    + " This has made a lot of people very angry and been widely regarded as a bad move.\""
+    + "― Douglas Adams, The Restaurant at the End of the Universe <br>");
+
+
+document.write (4+5); //displaying expression
+
+
+
